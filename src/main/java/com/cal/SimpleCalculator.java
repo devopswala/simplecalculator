cd40c78c08d53c6cc27e390f04c0dd62a16a5904
devopswala <devopswala@gmail.com>
2024-03-30 22:29:48 +0000

    Updated pom.xml

--- src/main/java/com/cal/SimpleCalculator.java
+++ src/main/java/com/cal/SimpleCalculator.java
@@ -2,11 +2,24 @@ package com.cal;
 
 public class SimpleCalculator {
 
+    // Example methods for addition and subtraction
     public int add(int a, int b) {
         return a + b;
     }
 
     public int subtract(int a, int b) {
         return a - b;
+    }
+
+    // Main method - Entry point of the application
+    public static void main(String[] args) {
+        SimpleCalculator calculator = new SimpleCalculator();
+
+        // Example usage of the calculator
+        int sum = calculator.add(5, 3);
+        int difference = calculator.subtract(5, 3);
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
     }
 }
