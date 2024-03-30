e06b79dbd23ed4837e9528cbc381c16f5149a9d6
devopswala <devopswala@gmail.com>
2024-03-30 23:28:26 +0000

    Adding surefire plugin

--- src/test/java/com/cal/SimpleCalculatorTest.java
+++ src/test/java/com/cal/SimpleCalculatorTest.java
@@ -3,6 +3,8 @@ package com.cal;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
+import main.java.com.cal.SimpleCalculator;
+
 public class SimpleCalculatorTest {
 
     private SimpleCalculator calculator = new SimpleCalculator();
@@ -16,4 +18,15 @@ public class SimpleCalculatorTest {
     public void testSubtract() {
         assertEquals(1, calculator.subtract(3, 2));
     }
+
+    @Test
+    public void add_ShouldReturnSum_WhenBothPositive() {
+        assertEquals(5, calculator.add(2, 3));
+    }
+
+    @Test
+    public void add_ShouldReturnSum_WhenOneNegative() {
+        assertEquals(1, calculator.add(2, -1));
+    }
+
 }
