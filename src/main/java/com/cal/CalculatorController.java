d2dd818242e136aa1a30e69da02b8f1f4651b296
devopswala <devopswala@gmail.com>
2024-03-31 18:28:12 +0100

    Adding sonar analysis stage

--- src/main/java/com/cal/CalculatorController.java
+++ src/main/java/com/cal/CalculatorController.java
@@ -1,5 +1,26 @@
-package main.java.com.cal;
+package com.cal
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+@Controller
 public class CalculatorController {
 
+    @GetMapping("/")
+    public String greeting() {
+        return "calculator";
+    }
+
+    @GetMapping("/calculate")
+    public String calculate(@RequestParam int a, @RequestParam int b, @RequestParam String operation, Model model) {
+        SimpleCalculator calculator = new SimpleCalculator();
+        int result = switch (operation) {
+            case "add" -> calculator.add(a, b);
+            case "subtract" -> calculator.subtract(a, b);
+            default -> 0;
+        };
+        model.addAttribute("result", result);
+        return "calculator";
+    }
 }
